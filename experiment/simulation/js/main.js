bf7e75e8dc8a940f9c8f5ad600c3ed90d77917f3
Varun Pal Singh <singhvarun928@gmail.com>
2023-11-14 12:49:49 +0530

    step2

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,17 @@
-//Your JavaScript goes in here
+const nextToStep1 = document.querySelector("#nextToStep1");
+const mainStep = document.querySelector("#mainStep");
+
+// step 1
+const step1 = document.querySelector("#step1");
+const instructionsStep1 = document.querySelector("#instructionsStep1");
+
+addEventListener("DOMContentLoaded", (evt) => {
+    nextToStep1.style.visibility = "visible";
+});
+
+nextToStep1.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    mainStep.style.visibility = "hidden";
+    step1.style.visibility = "visible";
+    instructionsStep1.textContent = "content...";
+});
