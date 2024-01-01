f3517e385e062405e27840f36da9168714beb8e6
Varun Pal Singh <singhvarun928@gmail.com>
2024-01-01 22:48:33 +0530

    started step7

--- experiment/simulation/js/step7.js
+++ experiment/simulation/js/step7.js
@@ -5,8 +5,18 @@ const simulationContainer = document.querySelector(".simulation-container");
 const pngColor = "#dbe3e9", gifColor = "#deebf7";
 
 const png1Step7 = document.querySelector("#png1Step7");
+const png2Step7 = document.querySelector("#png2Step7");
+const png3Step7 = document.querySelector("#png3Step7");
+const png4Step7 = document.querySelector("#png4Step7");
+const png5Step7 = document.querySelector("#png5Step7");
+const png6Step7 = document.querySelector("#png6Step7");
 
-const trans1Step7 = document.querySelector("#trans6Step7");
+const trans1Step7 = document.querySelector("#trans1Step7");
+const trans2Step7 = document.querySelector("#trans2Step7");
+const trans3Step7 = document.querySelector("#trans3Step7");
+const trans4Step7 = document.querySelector("#trans4Step7");
+const trans5Step7 = document.querySelector("#trans5Step7");
+const trans6Step7 = document.querySelector("#trans6Step7");
 
 const arr1Step7 = document.querySelector("#arr1Step7");
 
